d47cd78df617187a7c4d48581c1e8c2eb87647a7
Diego Palacios <escalerad@uninorte.edu.co>
2021-08-30 21:29:06 -0500

    actualizando avances

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hola mundo</h1>
+
+      {/* Sidebar */}
+      <Sidebar/>
+
+      {/* Feed */}
+
+      {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
